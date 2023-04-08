62319affd533266aa668479c5af74a5c01216a05
Ivanov V <64227404+penOpen@users.noreply.github.com>
2023-04-09 02:18:15 +0300

    Add project files.

---
+++ Methods.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using MailKit;
+using MailKit.Net.Imap;
+using MailKit.Search;
+using MimeKit;
+using ExcelDataReader;
+using System.Data;
+
+public class UserInfo
+{
+    public string? mail;
+    public string? password;
+
+    public UserInfo(string? mail, string? password)
+    {
+        this.mail = mail;
+        this.password = password;
+    }
+}
+
+
+public class Methods
+{
+    public static UserInfo GetUserInfo()
+    {
+        System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+        using var stream = File.Open(@"../../../data.xlsx", FileMode.Open, FileAccess.Read);
+        using var reader = ExcelReaderFactory.CreateReader(stream);
+        var result = reader.AsDataSet(new ExcelDataSetConfiguration()
+        {
+            ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
+            {
+                UseHeaderRow = true
+            }
+        });
+        var table = result.Tables[0];
+        string? mail = table.Rows[0][1].ToString();
+        string? password = table.Rows[1][1].ToString();
+        stream.Close();
+        var info = new UserInfo(mail, password);
+        Console.WriteLine("Loaded User Info");
+        return info;
+    }
+    public static Dictionary<string, string> GetFolders()
+    {
+        System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+        using var stream = File.Open(@"../../../data.xlsx", FileMode.Open, FileAccess.Read);
+        using var reader = ExcelReaderFactory.CreateReader(stream);
+        var result = reader.AsDataSet(new ExcelDataSetConfiguration()
+        {
+            ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
+            {
+                UseHeaderRow = true
+            }
+        });
+        var table = result.Tables[1];
+        var dictionary = new Dictionary<string, string>();
+        foreach (DataRow row in table.Rows)
+        {
+            string? key = row[0].ToString();
+            string? value = row[1].ToString();
+            if (key != null || value != null)
+            {
+                List<string> wordsList = value.Split(' ').ToList();
+                foreach (var word in wordsList)
+                {
+                    dictionary.Add(word.ToUpper(), key);
+                }
+            }
+
+        }
+        stream.Close();
+        Console.WriteLine("Loaded Folders and Keys");
+        return dictionary;
+    }
+
+    public static ImapClient ConnectToMail()
+    {
+        var client = new ImapClient();
+        try
+        {
+            client.Connect("imap.mail.ru", 993, true);
+            Console.WriteLine("Client connection: " + client.IsConnected);
+            return client;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Error: Invalid server");
+            Environment.Exit(-1);
+            throw; //try to fix
+        }
+    }
+
+    public static ImapClient AuthentificateUser(ImapClient client, UserInfo info)
+    {
+        try
+        {
+            client.Authenticate(info.mail, info.password);
+            Console.WriteLine("Client Authentification: " + client.IsAuthenticated);
+            return client;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Error: invalid mail or password");
+            Environment.Exit(-1);
+            throw; //try to fix
+        }
+    }
+
+    public static void CheckFolders(ImapClient client, Dictionary<string, string> dictionary)
+    {
+        HashSet<string> folders = new HashSet<string>(dictionary.Values);
+        var inbox = client.GetFolder("INBOX");
+        foreach (var folder in folders)
+        {
+            try
+            {
+                if (inbox.GetSubfolder(folder) != null)
+                {
+                    Console.WriteLine("Folder " + folder + " exists!");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Cant find folder " + folder);
+                var newFolder = client.GetFolder("INBOX");
+                newFolder.Create(folder, false);
+                Console.WriteLine("Created folder " + folder);
+            }   
+        }
+
+        try
+        {
+            if (inbox.GetSubfolder("Unsorted") != null)
+            {
+                Console.WriteLine("Folder Unsorted exists!");
+            }
+        }
+        catch (Exception)
+        {
+            var newFolder = client.GetFolder("INBOX");
+            newFolder.Create("Unsorted", false);
+            Console.WriteLine("Created folder Unsorted");
+        }
+    }
+    public static int MoveMessage(ImapClient client, UniqueId u, MimeMessage msg, Dictionary<string, string> dictionary)
+    {
+        var inbox = client.GetFolder("INBOX");
+        var subject = msg.Subject.ToUpper();
+        List<string> wordsList = subject.Split(' ').ToList();
+        foreach (var word in wordsList)
+        {
+            if (dictionary.ContainsKey(word))
+            {
+                var destination = dictionary[word];
+                inbox.MoveTo(u, inbox.GetSubfolder(destination));
+                Console.WriteLine("Message [UID: " + u + "] MOVED TO " + destination);
+                return 1;
+            }
+            else
+            {
+                inbox.MoveTo(u, inbox.GetSubfolder("Unsorted"));
+                Console.WriteLine("Message [UID: " + u + "] MOVED TO Unsorted");
+                return 0;
+            }
+        }
+
+        return -1;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using MailKit;
+using MailKit.Search;
+
+
+var cancellationTokenSource = new CancellationTokenSource();
+var cancellationToken = cancellationTokenSource.Token;
+
+var info = Methods.GetUserInfo();
+var dictionary = Methods.GetFolders();
+
+var client = Methods.ConnectToMail();
+client = Methods.AuthentificateUser(client, info);
+
+var inbox = client.Inbox;
+inbox.Open(FolderAccess.ReadWrite);
+var uids = inbox.Search(SearchQuery.All);
+
+Methods.CheckFolders(client, dictionary);
+
+Console.WriteLine("Listening on inbox messages");
+
+while (true)
+{
+    cancellationToken.WaitHandle.WaitOne(TimeSpan.FromSeconds(5));
+    var newUids = inbox.Search(SearchQuery.Not(SearchQuery.Uids(uids)));
+    if (newUids.Count > 0)
+    {
+        foreach (var u in newUids)
+        {
+            var msg = inbox.GetMessage(u);
+            Console.WriteLine("New message from" + msg.From + " With subject: " + msg.Subject);
+            uids.Add(u);
+            int result = Methods.MoveMessage(client, u, msg, dictionary);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Zero messages");
+    }
+
+    if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Q)
+    {
+        Console.WriteLine("Stop listening");
+        break;
+    }
+}
+
+client.Disconnect(true);
